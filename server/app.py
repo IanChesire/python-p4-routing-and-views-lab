bf3c8e1ccfadfd34d6039d13dd4e5617138f0591
Ian <iankosgey535@gmail.com>
2024-04-23 16:43:21 +0300

    Used numbers as integers and returned as str

--- server/app.py
+++ server/app.py
@@ -1,46 +1,41 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
-
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
 
 @app.route('/')
 def index():
     return '<h1>Python Operations with Flask Routing and Views</h1>'
 
-@app.route('/print/<parameter>')
+@app.route('/print/<string:parameter>')
 def print_string(parameter):
     print(parameter)
     return parameter
 
-@app.route('/count/<int:parameter>')
-def count(parameter):
-    numbers = '\n'.join(str(i) for i in range(parameter))
-    print (numbers)
-    return numbers
-    
-    
-@app.route('/math/<num1>/<operation>/<num2>')
+@app.route('/count/<int:number>')
+def count(number):
+    count = f''
+    for n in range(number):
+        count += f'{n}\n'
+    return count
 
-def math(num1, operation, num2):
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, num2, operation):
     if operation == '+':
-        result = num1 + num2
+        return str(num1 + num2)
+    
     elif operation == '-':
-        result = num1 - num2
-    elif operation == '*':
-        result = num1 * num2
-    elif operation == 'div':
-        if num2 == 0:
-            return 'Error: Division by zero'
-        result = num1 / num2
-    elif operation == '%':
-        result = num1 % num2
-    else:
-        return 'Invalid operation'
+        return str(num1 - num2)
 
-    return str(result)
-    
-    
+    elif operation == '*':
+        return str(num1 * num2)
+
+    elif operation == 'div':
+        return str(num1 / num2)
+
+    elif operation == '%':
+        return str(num1 % num2)
+
+    return 'Operation not recognized. Please use one of the following: + - * div %'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
